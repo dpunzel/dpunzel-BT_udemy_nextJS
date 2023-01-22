56eee129c7144e8035d6359ecb3dafe8ae4cb0db
David Punzel <dpunzel15@georgefox.edu>
2023-01-21 21:15:20 -0800

    Layout, Header and footer

--- dj-events-frontend/pages/events/[slug].js
+++ dj-events-frontend/pages/events/[slug].js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Layout from '../../components/Layout'
 
 export default function EventPage() {
 
   return (
-    <div>
+    <Layout>
       <h1>My Event</h1>
-    </div>
+    </Layout>
   )
 }
